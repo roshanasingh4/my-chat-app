6d80cc6220570696652775240a955f5c6e0c3be3
Roshan Singh <roshanasingh4@gmail.com>
2021-08-18 16:24:20 +0530

    Enter

---
+++ discord_bot/main.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+console.log('roshan is online');
+});
+
+
+
+client.login('ODc3MTUwMTM3NjcwMDY2MjA3.YRubuQ.DeIn_-y0fRGPKQzD45fZjxrssdA');
